918e410787527885585f316f79f58a5126ea401f
dabaozhu <380067052@qq.com>
2023-08-17 20:26:06 +0800

    modcode and add CMakLists

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include"../include/add.h"
 #include"../include/minus.h"
+void add(int* add1,int* add2){
+            printf("请输入第一个加数：\n");      
+            scanf("%d",add1);      
+            printf("请输入第二个加数：\n"); 
+            scanf("%d",add2);        
+            printf("%d + %d = %d\n",*add1,*add2,addFunc(*add1,*add2));
+}
+void minus(int* minus1,int* minus2){
+             printf("请输入被减数\n");  
+             scanf("%d",minus1);
+             printf("请输入减数\n");  
+             scanf("%d",minus2);
+             printf("%d - %d = %d\n",*minus1,*minus2,minusFunc(*minus1,*minus2));    
+}
 int main(int argc,void* argv[]){
     int add1 = 0;
     int add2 =0;
@@ -11,22 +25,15 @@ int main(int argc,void* argv[]){
     printf("1:加法\n");
     printf("2:减法\n");
     
-    scanf("请输入：%d\n",&option);
+    scanf("%d",&option);
     switch (option)
     {
     case 1:
-
-        scanf("请输入第一个加数：%d\n",&add1);
-        scanf("请输入第二个加数：%d\n",&add2);
-        printf("%d + %d = %d\n",add1,add2,addFunc(add1,add2));
+        add(&add1,&add2);
         break;
     case 2:
-
-    scanf("请输入被减数：%d\n",&minus1);
-    scanf("请输入减数：%d\n",&minus2);
-    printf("%d - %d = %d\n",minus1,minus2,minusFunc(minus1,minus2));
-    break;
-    
+        minus(&minus1,&minus2);
+        break;
     default:
         break;
     }
